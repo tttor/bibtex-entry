53cf7a7f44fccb44a643e2d7165b4dee36b58205
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-11-23 20:44:44 +1000

    init find cite keys

--- merge.py
+++ merge.py
@@ -7,12 +7,39 @@ def main():
     assert len(sys.argv)==2
     outbibfpath = sys.argv[1]
     indir = os.path.dirname(os.path.realpath(__file__))
+    outdir = os.path.dirname(outbibfpath)
+
+    cite_keys = []
+    for fname in os.listdir(outdir):
+        if '.tex' not in fname: continue
+        cite_keys += find(os.path.join(outdir, fname))
+    cite_keys = list(set(cite_keys))
 
     with open(outbibfpath, 'w') as outfile:
         for fname in os.listdir(indir):
             if fname in ['merge.py', 'README.md', 'LICENSE', '.git']: continue
             with open(os.path.join(indir, fname), 'r') as infile:
                 outfile.write(infile.read())
+
+def find(fpath):
+    keys = ['\cite{', '\citep{']
+    cite_keys = []
+    with open(fpath, 'r') as f:
+        for row in f:
+            row = row.strip()
+            for k in keys:
+                cnt = row.count(k)
+                if cnt==0: continue
+
+                start_idx = 0
+                for c in range(cnt):
+                    start_idx = row.find(k, start_idx)
+                    end_idx = row.find('}', start_idx )
+                    cite_str = row[start_idx:end_idx].replace(k,'')
+                    cite_keys += [i.strip() for i in cite_str.split(',')]
+                    start_idx = end_idx+1
+    return cite_keys
+
 
 if __name__ == '__main__':
     main()
