43df0223f49e0e84f64c9d029f727470f8dd9394
Vektor Dewanto <vektor.dewanto@gmail.com>
2020-10-28 08:07:55 +1000

    fix

--- push.sh
+++ push.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # git remote add originbitbucket git@bitbucket.org:tttor/bibtex-entry.git
 # git remote add origin git@gitlab.com:tttor/bibtex-entry.git
+# https://askubuntu.com/questions/370697/how-to-count-number-of-files-in-a-directory-but-not-recursively
+
+echo '=== n entries ==='
+find ./entry -maxdepth 1 -type f | wc -l
 
 echo '=== committing ==='
 git add --all
